803ba4dbb0a322ae335bfba36239743b06bd46b8
dracohu2025 <dracohu2025@gmail.com>
2026-06-04 13:04:54 +0800

    fix: reduce X Articles max images limit from 26 to 25

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,6 +1,6 @@
 const helperBase = "http://127.0.0.1:49231";
 const XAP_CONTENT_VERSION = "context-safe-storage-v2";
-const X_ARTICLE_MAX_IMAGES = 26;
+const X_ARTICLE_MAX_IMAGES = 25;
 const FEISHU_MEDIA_TIMEOUT_MS = 15_000;
 const FEISHU_MEDIA_MAX_ATTEMPTS = 3;
 
